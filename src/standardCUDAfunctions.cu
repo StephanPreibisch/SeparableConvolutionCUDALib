3dbdd203eede687362d7c8f89d32b6bc28e4ebbd
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-08-06 18:02:58 -0400

    added code for running the code on the GPU using CPU's, is commented out

--- src/standardCUDAfunctions.cu
+++ src/standardCUDAfunctions.cu
@@ -11,36 +11,40 @@
 //==============================================
 int getCUDAcomputeCapabilityMajorVersion(int devCUDA)
 {
+	//return 3;
 	int major = 0, minor = 0;
 	cuDeviceComputeCapability(&major, &minor,devCUDA);
-
 	return major;
 }
 int getCUDAcomputeCapabilityMinorVersion(int devCUDA)
 {
+	//return 0;
 	int major = 0, minor = 0;
 	cuDeviceComputeCapability(&major, &minor,devCUDA);
-
 	return minor;
 }
 
 int getNumDevicesCUDA()
 {
+	//return 2;
 	int count = 0;
 	if ( !HANDLE_ERROR_NOCRASH(cudaGetDeviceCount ( &count )) )
 		return -1;
-
 	return count;
 }
 void getNameDeviceCUDA(int devCUDA, char* name)
 {
+	//if ( devCUDA == 0 )
+	//	strcpy(name, "CPU emulation #1");
+	//else
+	//	strcpy(name, "CPU emulation #2");
 	cudaDeviceProp prop;
 	HANDLE_ERROR( cudaGetDeviceProperties(&prop, devCUDA));
-
 	memcpy(name,prop.name,sizeof(char)*256);
 }
 long long int getMemDeviceCUDA(int devCUDA)
 {
+	//return (long long int)3 * (long long int)1024 * (long long int)1024 * (long long int)1024;
 	cudaDeviceProp prop;
 	HANDLE_ERROR( cudaGetDeviceProperties(&prop, devCUDA));
 	return ((long long int)prop.totalGlobalMem);
